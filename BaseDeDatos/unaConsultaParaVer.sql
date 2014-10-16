62c76984fadbce9174352ab1233c91655d608721
KevinZ01 <pegasoveloz001>
2014-10-15 21:12:54 -0400

    tu parte Ale,que me pediste que lo suba

---
+++ BaseDeDatos/unaConsultaParaVer.sql
@@ -0,0 +1,7 @@
+﻿SELECT dia, horaini, horafin, nombreambiente, nombredisertante
+FROM
+	(SELECT disertante_iddisertante, periodo_idperiodo, ambiente_idambiente, dia
+	FROM
+		(SELECT idcurso FROM curso WHERE tema = 'sis2') AS uno, clase
+	WHERE uno.idcurso = clase.curso_idcurso) AS dos, disertante, ambiente, periodo
+WHERE dos.disertante_iddisertante = disertante.iddisertante and dos.periodo_idperiodo = periodo.idperiodo and dos.ambiente_idambiente = ambiente.idambiente
